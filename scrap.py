8774cb4a409923975af61ab8808d1abbb93fff3d
WissemBT <wissembettaieb2000@gmail.com>
2023-09-03 02:22:21 +0200

    good proxies verifying

--- scrap.py
+++ scrap.py
@@ -2,16 +2,39 @@ import requests
 
 proxies_list = open("rotating_proxies_list.txt","r").read().strip().split("\n")
 
+proxies = set(proxies_list)
+good_proxy = set()
+bad_proxy = set()
+
 VALID_STAT = [200,301,302,307,404]
 def get(url,proxy):
 	try:
 		response=requests.get(url,proxies={'http': f"http://{proxy}"},timeout=30)
 		if response.status_code in VALID_STAT:
-			print(response.status_code,response.text)
+			#print(response.status_code,response.text)
+			set_good(proxy)
+		else:
+			set_bad(proxy)
 	except Exception as e:
-		print(e)
+		set_bad(proxy)
 def check_proxies():
-	for proxy in proxies_list:
+	for proxy in list(proxies):
 		get("http://ident.me/",proxy)
 
+def reset_proxy(proxy):
+	proxies.add(proxy)
+	good_proxy.discard(proxy)
+	bad_proxy.discard(proxy)
+def set_good(proxy):
+	proxies.discard(proxy)
+	good_proxy.add(proxy)
+	bad_proxy.discard(proxy)
+def set_bad(proxy):
+	proxies.discard(proxy)
+	good_proxy.discard(proxy)
+	bad_proxy.add(proxy)
 check_proxies()
+
+print("not used : ", proxies)
+print("good proxies : ", good_proxy)
+print("bad proxies : ",bad_proxy)
